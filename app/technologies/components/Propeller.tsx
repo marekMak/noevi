0f0a11f85a4e6ea086affb38bc20b2434b8f2799
karlovkar <spravca@karlovaves.sk>
2025-06-06 14:18:51 +0200

    Oprava chýb a pridanie novej funkcie propeller

--- app/technologies/components/Propeller.tsx
+++ app/technologies/components/Propeller.tsx
@@ -3,9 +3,24 @@ import { useFrame } from "@react-three/fiber";
 import { forwardRef, useRef } from "react";
 import { Group } from "three";
 import * as THREE from "three";
+import { GLTF } from "three-stdlib";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Circle: THREE.Mesh;
+    Circle001: THREE.Mesh;
+    Cube: THREE.Mesh;
+    Cube001: THREE.Mesh;
+    Cube001_1: THREE.Mesh;
+  };
+  materials: {
+    windmill: THREE.Material;
+    windmillGreen: THREE.Material;
+  };
+};
 
 const Propeller = forwardRef<Group>((_, ref) => {
-  const { nodes, materials } = useGLTF("/models/propeller.gltf") as any;
+  const { nodes, materials } = useGLTF("/models/propeller.gltf") as GLTFResult;
   const propeller = useRef<THREE.Mesh>(null);
 
   useFrame(() => {
@@ -17,35 +32,46 @@ const Propeller = forwardRef<Group>((_, ref) => {
   return (
     <group
       ref={ref}
-      dispose={null}
       scale={0.5}
       rotation={[0, -Math.PI * 0.25, 0]}
       position={[1.5, 0, 0]}
-      receiveShadow
-      castShadow
     >
       <mesh
         ref={propeller}
-        position={[0, 0, 0.5]}
         geometry={nodes.Circle.geometry}
-        material={materials["windmill"]}
+        material={materials.windmill}
+        position={[0, 0, 0.5]}
+        castShadow
+        receiveShadow
       />
-
       <mesh
         geometry={nodes.Circle001.geometry}
-        material={materials["windmill"]}
-      ></mesh>
+        material={materials.windmill}
+        castShadow
+        receiveShadow
+      />
       <mesh
         geometry={nodes.Cube001.geometry}
-        material={materials["windmill"]}
+        material={materials.windmill}
+        castShadow
+        receiveShadow
       />
-      <mesh geometry={nodes.Cube.geometry} material={materials["windmill"]} />
+      <mesh
+        geometry={nodes.Cube.geometry}
+        material={materials.windmill}
+        castShadow
+        receiveShadow
+      />
       <mesh
         geometry={nodes.Cube001_1.geometry}
-        material={materials["windmillGreen"]}
+        material={materials.windmillGreen}
+        castShadow
+        receiveShadow
       />
     </group>
   );
 });
+
+useGLTF.preload("/models/propeller.gltf");
 
 export default Propeller;
